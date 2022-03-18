b83cd49dc1aeface5bac9f124ada991e286cdc42
Rohidul <rohidul209@gmail.com>
2022-03-18 11:59:07 +0600

    footer site name error fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="border-t border-gray-100 dark:border-gray-800">
       <Container>
         <div className="py-8">
-          <p className="uppercase">© 2022 {SITE_NAME.replaceAll(" ", "")}</p>
+          <p className="uppercase">© 2022 {SITE_NAME}</p>
         </div>
       </Container>
     </footer>
